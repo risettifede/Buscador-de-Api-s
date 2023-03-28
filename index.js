4dea0d63f9845c665b944a67ac5b6dc0540782ad
Fede Risetti <115749116+risettifede@users.noreply.github.com>
2023-03-28 09:05:49 -0300

    new commit

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+
 const OPTIONS = {
   method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '2540e85d64msh8542e13520f6cdfp1b717ajsn1c6ce2f605f9',
-    'X-RapidAPI-Host': 'ipstack1.p.rapidapi.com'
+  // headers: {
+  //   'X-RapidAPI-Key': '2540e85d64msh8542e13520f6cdfp1b717ajsn1c6ce2f605f9',
+  //   'X-RapidAPI-Host': 'ipstack1.p.rapidapi.com'
+  // }
+}
+
+const fetchIpInfo = async (id) => {
+  try{
+    const getInfo = await fetch(`https://dummyjson.com/products/${id}`, OPTIONS)
+    const data =  getInfo.json()
+    console.log(data)
+    return data
+  }catch(err){
+    console.log(err)
   }
 }
 
-const fetchIpInfo = ip => {
-  fetch(`https://ipstack1.p.rapidapi.com/api.ipstack.com/${ip}`, OPTIONS)
-    .then(res => res.json())
-    .catch(err => console.error(err))
-}
 
 // const $ = selector => document.querySelector(selector)
 // const $form = $("#form") 
@@ -21,23 +28,55 @@ const $input = document.querySelector("#input")
 const $sumbit = document.querySelector("#sumbit")
 const $results = document.querySelector("#results")
 
-$form.addEventListener("sumbit", async (event) => {
-  event.preventDefault() // Es para evitar que se refresque toda la página
-  const { value } = $input
-  if (!value) return
+
+
+async function logSomething(e) {
+  e.preventDefault() // Es para evitar que se refresque toda la página
+  console.log('something');
+  const { value: id } = $input
+
+
+  if (!id) return
 
   $sumbit.setAttribute('disabled', ' ')
   $sumbit.setAttribute('aria-busy', 'true')
 
 
-  const ipInfo = await fetchIpInfo(value)
+  const productInfo = await fetchIpInfo(id)
 
-  if (ipInfo) {
-    $results.innerHTML = JSON.strindify(ipInfo, null, 2)
-  }
+  console.log(productInfo)
 
-  $sumbit.removeAttribute('disabled')
-  $sumbit.removeAttribute('aria-busy')
+  // if (ipInfo) {
+  //   $results.innerHTML = JSON.strindify(ipInfo, null, 2)
+  // }
+
+  // $sumbit.removeAttribute('disabled')
+  // $sumbit.removeAttribute('aria-busy')
 
 
-})
+
+}
+
+$form.addEventListener('submit', logSomething)
+
+// $form.addEventListener("sumbit", async (event) => {
+//   console.log("ejecutando");
+//   const { value } = $input
+//   if (!value) return
+
+//   $sumbit.setAttribute('disabled', ' ')
+//   $sumbit.setAttribute('aria-busy', 'true')
+
+
+//   const ipInfo = await fetchIpInfo(value)
+
+//   if (ipInfo) {
+//     $results.innerHTML = JSON.strindify(ipInfo, null, 2)
+//   }
+
+//   $sumbit.removeAttribute('disabled')
+//   $sumbit.removeAttribute('aria-busy')
+
+//   event.preventDefault() // Es para evitar que se refresque toda la página
+
+// })
